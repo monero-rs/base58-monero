223e477567defa7c14ca2510c7f0f756a727e0af
Joel <joel@truelevel.io>
2019-03-09 01:27:14 +0100

    Add MIT header in files

--- src/base58.rs
+++ src/base58.rs
@@ -1,3 +1,18 @@
+// Rust Monero Base58 Library
+// Written in 2019 by
+//   h4sh3d <h4sh3d@truelevel.io>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+
 use std::num::Wrapping;
 use std::{error, fmt};
 use std::fmt::Display;
